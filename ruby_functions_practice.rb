3e6815bc6c01304af8a52f4f8e4010786fd86e30
Chris Haining <chrishaining@yahoo.co.uk>
2019-07-31 09:58:27 +0100

    updates file with comments following classroom review of homework

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -32,7 +32,7 @@ def add_string_as_number(first_str1, second_str2)
   return first_num + second_num
 end
 
-#The month functions were created as hashes, though I'm not sure we'd bother putting hashes inside functions.
+#The month functions were created as hashes, though I'm not sure we'd bother putting hashes inside functions. I would have gone for cases
 def number_to_full_month_name(month_number)
   month_name = {1 => "January",
                 3 => "March",
@@ -40,6 +40,8 @@ def number_to_full_month_name(month_number)
   return month_name[month_number]
 end
 
+#for this one, we could have used the .slice method on the full_month_name.
+# sliced_month_name=number_to_full_month_name(month_number).slice(0,3)
 def number_to_short_month_name(month_number)
   month_name = {1 => "Jan",
                 4 => "Apr",
@@ -51,8 +53,10 @@ def volume_of_cube(side)
   return side ** 3
 end
 
+#instead of 3.14, we could have used Math::PI. It would be better to do the rounding at the end.
 def volume_of_sphere(radius)
   volume = 4/3.round(2) * 3.14 * radius**3
+  #volume = (4.0 / 3.0) * Math::PI * (radius**3)
   return volume.to_i
 end
 
